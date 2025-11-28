38367fdf824e1a7b1300e44fd9ad85d16467463d
Alessandro Bonanno <alessandro.bonanno@edreamsodigeo.com>
2025-11-28 16:57:06 +0100

    feat: update

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,19 +1,2 @@
-import { locales } from '../locales/data.js'
-import { browser } from '$app/environment'
-import { loadLocale } from 'wuchale/load-utils'
-import '../locales/main.loader.svelte.js'
-
-import type { LayoutLoad } from './$types.js'
-
-import { get } from 'svelte/store'
-import { language } from '$lib/stores.js'
-
-export const load: LayoutLoad = async () => {
-  const locale = get(language)
-  if (browser && locales.includes(locale)) {
-    await loadLocale(locale)
-  }
-}
-
 export const prerender = true;
 export const trailingSlash = 'always';
