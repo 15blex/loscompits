c873625b75e5da8815eccee393b5501134dd4e6e
Alexi <15blex@gmail.com>
2025-11-28 17:43:08 +0100

    feat: update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,6 @@
 
 <section>
   <h1>Cumbia, Salsa y Son Cubano</h1>
-  <p>
-    Los Compit's es una banda, la cual se fundó y nos encontramos tanto en la calle y fuera de nuestras tierras. Sin embargo nos consideramos Carnales. Países como México y Argentina y también de distintas Provincias... Como por ejemplo Cordoba (Argentina), Mar de Plata (Argentina), Monterrey (México), San Luis Potosí (México), Distrito Federal (México) y también de las afueras de la ciudad (D.F.). Intentando mandar un mensaje: tanto musical pero a la vez Social sin camuflajear nuestros Principios
-  </p>
-  <p>
-    En este proyecto nos interesa reunir nuestras facultades y conseguir expandir nuestra música El nombre de esto fue elegido de la amistad. Es por ello que nos llamamos Los Compit's
-  </p>
 </section>
 
 <style>
--- src/routes/bio/+page.svelte
+++ src/routes/bio/+page.svelte
@@ -5,22 +5,9 @@
 <section id="bio-section">
   <h1>Biografia</h1>
   <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, sapiente cumque! Asperiores hic laudantium doloribus, illum eligendi nihil? Ad assumenda, cumque harum aperiam omnis et consequatur sapiente illum soluta accusamus!
-    Voluptatem omnis dolorem quos, animi maxime numquam nam dolore repellat esse, molestias repellendus consectetur illo ipsum alias ut perferendis nobis facere architecto fugiat eaque, iste quia assumenda aliquid! Laudantium, eaque.
-    Itaque inventore iusto, quisquam tempore labore quo repellendus repellat et animi suscipit qui ad maxime assumenda quidem, ipsum libero asperiores natus facilis! Accusantium molestiae, iste dolorem reprehenderit voluptatum animi ab.
-    Dolore ducimus repellendus ipsum animi eaque alias nulla dicta sunt minima. Commodi velit iure enim id quam, facere maiores doloribus amet. Sequi labore magnam cumque alias deserunt fugiat quia numquam.
+    Este grupo se forma en las calles de Barcelona trasladando cada uno de sus integrantes el sabor de sus tierras lanzando un mensaje de alegria y igualdad que no dejará indiferente a nadie.
   </p>
-
-  <h2>Seccion</h2>
   <p>
-    Eligendi aliquid molestiae doloribus dolor nesciunt, non saepe veniam corporis vel dolorem quam soluta vero amet quisquam at molestias! Beatae porro velit quam facilis! Odit voluptatem saepe repudiandae nam nobis?
-    Ullam officiis consequatur repellendus voluptates consequuntur sapiente cupiditate? Magni expedita qui inventore! Sed, repellat eius quod quibusdam qui at dolore doloribus molestias minima laborum sint sequi sapiente nemo mollitia aliquid?
-    Sit, corporis. Quos reprehenderit optio totam est dolor ad tempore deserunt natus ea. Voluptatem non corporis rem inventore, magnam dolor quae consectetur beatae blanditiis ea sapiente veniam voluptate, quidem quo?
-    Dolores dolor veniam in alias tenetur non vel unde magnam deleniti velit rem cum doloribus vero dolorum quo, aliquam sed doloremque quam consectetur aperiam quod magni repellendus placeat totam! Magni.
-  </p>
-  <h3>Otra Seccion</h3>
-  <p>  
-    Error harum, ipsa consectetur ab, earum quisquam natus iusto accusamus sapiente minus nobis expedita distinctio veniam? Nobis quae officiis, maxime quasi architecto consectetur eligendi nihil dolorum asperiores tempora ullam eveniet.
-    Blanditiis rerum, deleniti perspiciatis quam necessitatibus sapiente. Repudiandae, ab dolor tempora doloremque ut odio soluta, est unde ipsa totam ex officiis repellat expedita beatae rerum reprehenderit quam hic quo nulla!
+    El nombre de la banda define el carnalismo amistad y buena onda que representan siendo de diferentes partes del mundo y sin tener lazos de sangre (los compit’s) nos harán bailar y olvodarnos de los prejuicios que envuelven este mundo.
   </p>
 </section>
