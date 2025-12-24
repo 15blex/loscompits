c80641fb44102e7cf7f656fdb0a5ea1b5e5b7da6
Alessandro Bonanno <15blex@gmail.com>
2025-12-24 17:10:07 +0100

    feat(components): add header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,13 +1,40 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import { page } from '$app/stores';
+  import logo from '$lib/assets/logo.jpg';
+
+  const links = [
+    { name: 'Logo', href: '/', isLogo: true },
+    { name: 'Band', href: '/band' },
+    { name: 'Bio', href: '/bio' },
+    { name: 'Media', href: '/media' },
+    { name: 'Events', href: '/events' }
+  ];
+</script>
 
 <header>
   <nav>
     <ul>
-      <li><a href="/">Logo</a></li>
-      <li><a href="/band">Band</a></li>
-      <li><a href="/bio">Bio</a></li>
-      <li><a href="/media">Media</a></li>
-      <li><a href="/events">Events</a></li>
+      {#each links as link}
+        <li>
+          <a 
+            class:active={$page.url.pathname === link.href} 
+            href={link.href}
+          >
+            {#if link.isLogo}
+              <img src={logo} alt="Los Compits Logo">
+            {:else}
+              {link.name}
+            {/if}
+          </a>
+        </li>
+      {/each}
     </ul>
   </nav>
 </header>
+
+<style>
+  img {
+    block-size: 75px;
+    border-radius: var(--radius-full);
+  }
+</style>
